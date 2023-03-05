0b2d900dcc39e647bb943692f618f7b5b1beaeae
Sathwic97 <sathwic97@gmail.com>
2023-03-06 00:15:17 +0530

    added global css elements for heroButton class

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {}
 
@@ -15,7 +16,7 @@ export default function Hero({}: Props) {
         delaySpeed: 3000,
     });
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden z-50'>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircles/>
 
         <Image className='relative rounded-full h-32 w-32 mx-auto object-cover '
@@ -23,13 +24,34 @@ export default function Hero({}: Props) {
         alt="my image"
         width="200"
         height="200"/>
-        <div>
-            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Front-End Developer</h2>
-        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
-<span className='mr-3'>{text}</span>
-<Cursor cursorColor='#39FF14'/>
-</h1></div>
-    </div>
+        <div className='z-50'>
+         <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Front-End Developer</h2>
+         <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+         <span className='mr-3'>{text}</span>
+         <Cursor cursorColor='#39FF14'/>
+         </h1>
+         <div className='pt-5'>
+   <Link href="#about">        
+<button className='heroButton'>About</button>
+</Link> 
+<Link href="#experience">
+<button className='heroButton'>Experience</button>
+</Link>
+<Link href="#skills">
+<button className='heroButton'>Skills</button>
+</Link>
+<Link href="#projects">
+<button className='heroButton'>Projects</button>
+</Link>
+
+
+         </div>
+
+
+</div>
+
+</div>
+   
   )
 }
 
