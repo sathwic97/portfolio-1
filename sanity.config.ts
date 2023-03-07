075512339a37ac640f745f46786319879240e51a
Sathwic97 <sathwic97@gmail.com>
2023-03-07 15:00:17 +0530

    merged sanity and next

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,9 +3,15 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_ID!;
+const dataset = process.env.NEXT_PUBLIC_DATASET!;
+
 export default defineConfig({
-  name: 'default',
-  title: 'portfolio-1-build',
+  basePath:'/studio',
+  name: 'Sathwic_Portfolio_Studio',
+
+  title: 'Sathwic portfolio Studio',
 
   projectId: 'v5jmg2zj',
   dataset: 'production',
